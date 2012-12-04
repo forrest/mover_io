864006feccc4d7d1d167fe479c525c07e9fa89d9
Forrest Zeisler <forrest@getjobber.com>
2012-12-04 10:58:10 -0700

    adding warning about missing config file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,10 @@ require "mover_io"
 require "mocha"
 require 'rest-client'
 
-CONFIG = YAML::load(File.open('config.yml'))["test"]
+begin
+  CONFIG = YAML::load(File.open('config.yml'))["test"]
+rescue Errno::ENOENT
+  puts "\nIt looks like you haven't copied over sample.config.yml into a configuration file!\nRemote Specs will fail."
+  CONFIG = {}
+end
 
